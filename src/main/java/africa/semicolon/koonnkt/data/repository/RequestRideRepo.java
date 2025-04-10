754ff8bb52ee82c7d3aac7266b401c619f3254df
Abdulazeez <azeezmuritalaadeniyi@gmail.com>
2025-04-10 01:55:14 +0100

    change class injection

--- src/main/java/africa/semicolon/koonnkt/data/repository/RequestRideRepo.java
+++ src/main/java/africa/semicolon/koonnkt/data/repository/RequestRideRepo.java
@@ -4,9 +4,10 @@ import africa.semicolon.koonnkt.data.model.RequestRide;
 import africa.semicolon.koonnkt.data.model.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
-
+@Repository
 public interface RequestRideRepo extends JpaRepository<RequestRide, Long> {
     List<RequestRide> findByPassenger(Users passenger);
 }
